d6287adf9695fad2abed27e70bd321531641b3e5
bukolg <149950007+bukolg@users.noreply.github.com>
2023-11-05 18:50:34 +0300

    Тема2_лаба1

---
+++ TEST_2(испр)/laba1.py
@@ -0,0 +1,14 @@
+#Лаба_1
+arr = [10,20,None,40,50,60]
+sum=0
+k=0
+m=len(arr)
+print(arr)
+for i in range(m):
+    if arr[i]:
+        sum=sum+arr[i]
+    else:
+        k = i
+sar=sum/m
+arr[k] = sar
+print(arr)
