f4a3e6419cc7e64ca4a3ac59eb241c78da613e0f
bukolg <149950007+bukolg@users.noreply.github.com>
2023-11-05 18:11:41 +0300

    Add files via upload

---
+++ TEST_2/PrZad1.py
@@ -0,0 +1,12 @@
+rkr = int(input('Задайте радиус круга: '))
+akv = int(input('Задайте сторону квадрата: '))
+print (rkr, akv)
+PI = 3.141562
+Skr = PI * rkr ** 2
+Lokr = 2 * PI* rkr
+Pkv = 4 * akv
+Skv = akv ** 2
+print("Площадь круга", Skr)
+print("Длина окружности", Lokr)
+print("Периметр квадрата", Pkv)
+print("Площадь круга", Skv)
